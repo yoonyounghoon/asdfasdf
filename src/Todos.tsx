c4c8092400dee804621dfe687df25758be1cf472
yyounghoon <yhyoon@huray.net>
2021-12-29 11:11:41 +0900

    asdf

--- src/Todos.tsx
+++ src/Todos.tsx
@@ -8,7 +8,7 @@ function Todos() {
     );
 
     if (isLoading) {
-        return <p>"로딩 중...asdfasdasdfasdff"</p>;
+        return <p>"로딩 중...asdfasdasdfasdffasdfasdf"</p>;
     }
 
     if (error) {
