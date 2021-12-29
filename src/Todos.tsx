18ecd331371cf727ff0ad53851a372796278f9a8
“yyounghoon” <yhyoon@huray.net>
2021-12-29 10:57:53 +0900

    룰루랄라

--- src/Todos.tsx
+++ src/Todos.tsx
@@ -1,21 +1,21 @@
 import axios from "axios";
 import React from "react";
-import { useQuery } from "react-query";
+import {useQuery} from "react-query";
 
 function Todos() {
-  const {} = useQuery("todoData", () =>
-    axios.get("https://jsonplaceholder.typicode.com/todos/1")
-  );
+    const {} = useQuery("todoData", () =>
+        axios.get("https://jsonplaceholder.typicode.com/todos/1")
+    );
 
-  if (isLoading) {
-    return <p>"로딩 중..."</p>;
-  }
+    if (isLoading) {
+        return <p>"로딩 중...asdf"</p>;
+    }
 
-  if (error) {
-    return <p>"에러발생!!!"</p>;
-  }
+    if (error) {
+        return <p>"에러발생!!!"</p>;
+    }
 
-  return <div>{JSON.stringify(data)}</div>;
+    return <div>{JSON.stringify(data)}</div>;
 }
 
 export default Todos;
